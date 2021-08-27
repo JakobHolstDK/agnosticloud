3e5afe410d96336b435274dca1b382ffa89f532d
Jakob Holst <jho@miracle.dk>
2021-08-27 13:33:46 +0200

    prep

--- prepare_centos_8.sh
+++ prepare_centos_8.sh
@@ -1,15 +1,9 @@
 sudo dnf -y update
 sudo dnf  install qemu-kvm qemu-img virt-manager virt-viewer libvirt-daemon python3-pip  libvirt-client dnsmasq  python3-virtualenv pkg-config 
-
-
-
-exit
-
-
-sudo su -c "useradd miracle -s /bin/bash -m -g libvirt -G libvirt-qemu"
+sudo su -c "useradd miracle -s /bin/bash -m -g libvirt -G qemu"
 sudo mkdir -p /var/lib/virt-lightning/pool/upstream
-sudo chown -R libvirt-qemu:kvm /var/lib/virt-lightning/pool
-sudo chown -R miracle /var/lib/virt-lightning/pool/upstream
+sudo chown -R qemu:kvm /var/lib/virt-lightning/pool
+sudo chown -R miracle:qemu /var/lib/virt-lightning/pool/upstream
 sudo chmod 775 /var/lib/virt-lightning
 sudo chmod 775 /var/lib/virt-lightning/pool /var/lib/virt-lightning/pool/upstream
 
